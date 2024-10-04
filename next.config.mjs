3e4204d498d382c0b238d3ab1666c7eefd85f88b
“ayame0922” <aya.m.0922.1993@gmail.com>
2024-10-05 04:33:50 +0900

    vercelの画像最適化を無効

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, // 画像最適化を無効化する
     remotePatterns: [{ hostname: "raw.githubusercontent.com" }],
     domains: ["raw.githubusercontent.com"],
     deviceSizes: [320, 420, 768, 1024, 1200],
