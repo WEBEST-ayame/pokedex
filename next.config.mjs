a679f8613fd35eb48d5eb17a45531760f4b6c0ea
“ayame0922” <aya.m.0922.1993@gmail.com>
2024-11-02 05:27:31 +0900

    必要最低限の記述に限定

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // 画像最適化を無効化する
     remotePatterns: [{ hostname: "raw.githubusercontent.com" }],
-    domains: ["raw.githubusercontent.com"],
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
   },
 };
 
